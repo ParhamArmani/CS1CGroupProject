62e3d4665166d8dcb847700c0d98be45bd66f90e
Mr.Kon <yashkon@live.com>
2019-07-13 13:33:10 -0700

    Update shapes.h

--- shapes.h
+++ shapes.h
@@ -9,18 +9,40 @@ private:
     QPainter object;
     QPen penInfo;
     QBrush brushInfo;
-    void asd()
-    {
-//        penInfo.
-        object.
-    }
+    shapeType shape;
+    int x;
+    int y;
+    int h;
+    int w;
+    int id;
 public:
+    enum class ShapeType {noShape, line, polyline, polygon, rectangle, ellipse, text};
+
+    shape(QPaintDevice* device = nullptr, int id = -1; shapeType shape = shapeType::noshape);
     virtual ~shape();
 
-    virtual void draw() = 0;
-    virtual void move() = 0;
-    virtual void perimiter() = 0;
-    virtual void area() = 0;
+    ShapeType getShape() const;
+    const QPen& getPen() const;
+    const QBrush& getBrush() const;
+
+    void setShape(shapeType shape);
+    void setPen(Qt::GlobalColor, int width, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle);
+    void setPen(Qt::GlobalColor);
+    void setBrush(Qt::GlobalColor, Qt::BrushStyle);
+
+    void drawRect(int, int);
+
+    virtual void draw(const int translateX, const int translateY) = 0;        //draws the shape
+    virtual void move(const int newX, const int newY) = 0;        //updates the shapes
+    virtual double perimiter() = 0;   //finds the perimiter of the shape
+    virtual double area() = 0;        //finds the area of the shape
+
+    void setPenColor(QPen);         //
+    void setPenBrush(QBrush);       //
+    void setPenStyle(QPen);         //
+    void setCoords(int, int);       //
+    void setHeight(int);            //
+    void setWidth(int);             //
 };              //END SHAPE ABC
 
 
@@ -28,6 +50,10 @@ class rectangle : public shape
 {
 private:
     QRect rect;
+    void asd()
+    {
+        rect.;
+    }
 public:
     rectangle();    //defualt constructor
 
@@ -36,9 +62,44 @@ public:
 
 class line : public shape
 {
+private:
+    QLine line;
+
+public:
 
 };
 
-class polyline
+class polyline : public shape
+{
+private:
+
+public:
+};
+
+class polygon : public shape
+{
+private:
+
+public:
+
+};
+
+class elipse : public shape
+{
+private:
+    QRect rect;
+
+public:
+
+};
+
+class text : public shape
+{
+private:
+    QTextItem txt;
+public:
+
+};
+
 
 #endif // SHAPES_H
