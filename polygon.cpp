19c4accbf16f7ca389669c473c3f4d9c7b95d160
Parham AghamohammadiAmghani <Parham.Amghani@Gmail.com>
2019-07-23 13:14:24 -0700

    Doxygen Added

--- polygon.cpp
+++ polygon.cpp
@@ -2,11 +2,24 @@
 
 using namespace std;
 
+/**
+ * @brief Construct a new Polygon:: Polygon object (Default Constructor)
+ * 
+ * @param points : is a parameter of QPoint
+ * @param brush : is a parameter of Qbrush
+ * @param pen : is a parameter of Qpen
+ */
 Polygon::Polygon(vector<QPoint> points, const QBrush &brush, const QPen &pen)
         : PolyLine{move(points), brush, pen}
 {
 
 }
+
+/**
+ * @brief Construct a new Polygon:: Polygon object (Move Constructor)
+ * 
+ * @param move : is a parameter of type Polygon
+ */
 
 Polygon::Polygon(Polygon &&move) noexcept
 {
@@ -14,11 +27,22 @@ Polygon::Polygon(Polygon &&move) noexcept
     swap(points, move.points);
 }
 
+/**
+ * @brief Function to draw the Polygon Object
+ * 
+ * @param device: is a parameter of type QpaintDevice
+ */
 void Polygon::draw(QPaintDevice* device) const
 {
     auto paint = getPainter(device);
     paint->drawPolygon(points.data(), points.size());
 }
+
+/**
+ * @brief Function to retyrn the Area of a Polygon
+ * Pre: Recieves no parameters
+ * @return Area(Double) : Area of a polygon
+ */
 
 double Polygon::getArea()const
 {
@@ -31,8 +55,14 @@ double Polygon::getArea()const
 
         area += ((start.x()*start.y())-(start.y()*start.x()));
     }
+    return area;
 }
 
+/**
+ * @brief Function to return the Perimeter of the Polygon
+ * 
+ * @return Perimeter:  is value of type Double , represents the Perimeter of the Polygon.
+ */
 double Polygon::getPerimeter()const
 {
     double perimeter = 0;
@@ -44,4 +74,5 @@ double Polygon::getPerimeter()const
 
         perimeter += sqrt((first-second), (first-second));
     }
+    return perimeter;
 }
