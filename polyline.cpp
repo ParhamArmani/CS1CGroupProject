5897b8453775333b40630fe282f6a77343d2b654
Parham AghamohammadiAmghani <Parham.Amghani@Gmail.com>
2019-07-23 13:35:33 -0700

    Doxygen added

--- polyline.cpp
+++ polyline.cpp
@@ -2,30 +2,58 @@
 
 using namespace std;
 
-Polyline::Polyline(vector<QPoint> points, const QBrush &brush, const QPen &pen)
-                   :Shape{QPoint{}, brush, pen}, points{}}
-{
-    setCenter();
-}
+    /**
+     * @brief Construct a new Polyline:: Polyline object
+     * 
+     * @param points : is a parameter of type QPoint
+     * @param brush : is a parameter of type Qbrush
+     * @param pen  : is parameter of the type QPen
+     */
+    Polyline::Polyline(vector<QPoint> points, const QBrush &brush, const QPen &pen)
+                    :Shape{QPoint{}, brush, pen}, points{}}
+    {
+        setCenter();
+    }
 
+
+    /**
+     * @brief Construct a new Polyline:: Polyline object (Move constructor)
+     * 
+     * @param move : is a parameter of type Object Polyline
+     */
     Polyline::Polyline(Polyline &&move)noexcept
     {
         swap(move);
         swap(points, move.points);
     }
 
+    /**
+     * @brief Function to draw the Polyline
+     * 
+     * @param device : is a parameter of the QpaintDevice Type
+     */
     void Polyline::draw(QPaintDevice *device) const
     {
         auto paint = getPainter(device);
         paint->drawPolyline(points.data(), points.size());
     }
 
-    double Polyline::getArea() const
+    /**
+     * @brief Function to return the Area of the polyline (Default -1)
+     * 
+     * @return -1 :cPolyline has no area since it's not enclosed. 
+     */
+    double Polyline::area() const
     {
         return -1;
     }
 
-    double Polyline::getPerimeter() const
+    /**
+     * @brief Function to return the perimeter of the poliline object
+     * 
+     * @return -1 : Polyline has no perimeter since it's not enclosed. 
+     */
+    double Polyline::perimeter() const
     {
         return -1;
     }
